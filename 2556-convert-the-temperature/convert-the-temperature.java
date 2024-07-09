a11aaa91b23d587e5abf60bdd0057839f9cb3a41
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-07-09 09:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (15.34%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -1,13 +1,6 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        // double kel=celsius + 273.15;
-        // double fa=celsius * 1.80 + 32.00;
-        // DecimalFormat df=new DecimalFormat("#.#####");
-        // String str1=df.format(kel);
-        // String str2=df.format(fa);
         double arr[]=new double[2];
-        // arr[0]=Double.parseDouble(str1);
-        //  arr[1]=Double.parseDouble(str2);
         arr[0]=celsius + 273.15;
         arr[1]=celsius * 1.80 + 32.00;
         return arr;
