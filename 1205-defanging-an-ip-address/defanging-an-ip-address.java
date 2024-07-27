c88695c9135fd7aa036c7ff3f945115d063d67c0
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-07-27 12:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (15.65%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuffer str=new StringBuffer();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.')
+                str.append("[.]");
+            else
+                str.append(address.charAt(i));
+        }
+     return str.toString();
+    }
+   
+}
