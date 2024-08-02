baa3c7f01068e4156f49332781000e865572e472
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-08-02 16:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (83.88%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start=0;
+        int end=s.length-1;
+        char temp;
+        while(end>start){
+            temp=s[start];
+            s[start++]=s[end];
+            s[end--]=temp;
+        }
+    }
+}
