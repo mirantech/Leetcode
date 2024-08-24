36de8c35e546f5985e7e69bd1843550197d285cd
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-08-24 07:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (82.50%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,36 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<String> tree1=new ArrayList<>();
-        List<String> tree2=new ArrayList<>();
-        pre(tree1,p);
-        pre(tree2,q);
-       if(tree1.equals(tree2)){
-        return true;
-       }
-       return false;
+        return isSame(p, q);
     }
-    public static void pre(List<String> list,TreeNode root){
-        if(root==null){
-            list.add("null");
-            return;
+
+    private boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
         }
-        list.add(root.val+"");
-        pre(list,root.left);
-        pre(list,root.right);
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSame(p.left, q.left) && isSame(p.right, q.right);
     }
 }
