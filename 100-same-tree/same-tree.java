fbef44723abd8196a2e4caa52067724fed4a7380
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-08-24 07:38:45 +0530

    Time: 1 ms (100.00%) | Memory: 41.1 MB (26.61%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -17,20 +17,20 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         List<String> tree1=new ArrayList<>();
         List<String> tree2=new ArrayList<>();
-        post(tree1,p);
-        post(tree2,q);
+        pre(tree1,p);
+        pre(tree2,q);
        if(tree1.equals(tree2)){
         return true;
        }
        return false;
     }
-  public static void post(List<String> list, TreeNode root) {
-    if (root == null) {
-        list.add("null");
-        return;
+    public static void pre(List<String> list,TreeNode root){
+        if(root==null){
+            list.add("null");
+            return;
+        }
+        list.add(root.val+"");
+        pre(list,root.left);
+        pre(list,root.right);
     }
-    post(list, root.left);   // For inorder, this should come first
-    post(list, root.right);  // Then this for inorder
-    list.add(root.val + ""); // Add the root last for postorder
-}
 }
