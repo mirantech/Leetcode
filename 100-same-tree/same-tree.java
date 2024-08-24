38fc6cd37ce634974991497caed0726d98a42ab9
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2024-08-24 07:30:51 +0530

    Time: 1 ms (100.00%) | Memory: 40.9 MB (67.42%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,18 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return isSame(p, q);
+        List<String> tree1=new ArrayList<>();
+        List<String> tree2=new ArrayList<>();
+        post(tree1,p);
+        post(tree2,q);
+       if(tree1.equals(tree2)){
+        return true;
+       }
+       return false;
     }
-
-    private boolean isSame(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSame(p.left, q.left) && isSame(p.right, q.right);
+  public static void post(List<String> list, TreeNode root) {
+    if (root == null) {
+        list.add("null");
+        return;
     }
+    post(list, root.left);   // For inorder, this should come first
+    post(list, root.right);  // Then this for inorder
+    list.add(root.val + ""); // Add the root last for postorder
+}
 }
