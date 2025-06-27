507471aa0e0700939e7414e432dc00d22dac234d
MIRAN S S <145256247+mirantech@users.noreply.github.com>
2025-06-27 17:49:15 +0530

    Time: 4 ms (99.51%) | Memory: 45.3 MB (79.79%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        char ch[]=s.toCharArray();
         int i=0;
-        int j=ch.length-1;
+        int j=s.length()-1;
         while(i<j){
-            if(ch[i]!=ch[j]){
-                return (palin(i+1,j,ch)||(palin(i,j-1,ch)));  //elimitate one char i+1 or j-1 
+            if(s.charAt(i) != s.charAt(j)){
+                return (palin(i+1,j,s)||(palin(i,j-1,s)));  //elimitate one char i+1 or j-1 
             }
             i++;j--;
         }
         return true;
     }
-    public static boolean palin(int i,int j,char[] arr){
+    public static boolean palin(int i,int j,String s){
         while(i<j){
-            if(arr[i]!=arr[j]){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
